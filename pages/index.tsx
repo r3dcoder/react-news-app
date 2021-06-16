3b727f151aea6b0a8d24c961b8ddb89cc0ace83f
sdmahfuz <mahafuzul@algomatrix.co>
2021-06-17 00:14:57 +0600

    changed font size of index page's text

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ export default function Home() {
     <div className=" flex   items-center flex-col">
       <Toolbar />
       <div className="min-h-screen p-2 flex flex-col justify-center align-center text-center ">
-        <h1 className="font-bold">
+        <h1 className="font-bold text-3xl">
           Welcome to  React News App 
         </h1>
 
         <p>
-        <div className="font-bold m-2 cursor-pointer hover:bg-gray-500 " onClick= {()=>router.push('/feed/1')}>Get Latest News</div>
+        <div className="font-bold m-2 text-2xl cursor-pointer hover:bg-gray-500 " onClick= {()=>router.push('/article/feed')}>Get Latest News</div>
         </p>
       </div>
     </div>
